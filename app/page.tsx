5a4ff68661c13aca614a953e7b76f0d09b48822c
regisrex <ndizihiweregis06@gmail.com>
2024-05-23 21:13:41 +0200

    feat: add home

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Logo from "@/common/brand/Logo";
 import { Button } from "@/common/ui/button";
 import LoadingAnimation from "@/common/ui/loadingAnimation";
 import { Typography } from "@/common/ui/typography";
@@ -6,17 +5,19 @@ import { Typography } from "@/common/ui/typography";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center text-center justify-between p-24">
-      <h1 className="text-4xl font-black text-center text-emerald-1000">
-        Hello Nerd
-      </h1>
-      <Logo theme="dark" />
       <LoadingAnimation />
-      <Button variant={'primary'}> Hello World </Button>
-      <Button variant={'bordered'}> Hello World </Button>
-      <Button variant={'secondary'}> Hello World </Button>
-      <Button variant={'link'}> Hello World </Button>
-      <Typography variant={'title'} className="w-1/2">App hosting done like never before</Typography>
-      <Typography variant={'paragraph'} className="w-1/2">App hosting done like never before</Typography>
+      <h1 className="text-4xl font-black text-center bg-gradient-to-br from-emerald-600 to-emerald-400 bg-clip-text text-transparent">
+        Zepto
+      </h1>
+      {/* <Logo theme="dark" /> */}
+      <Typography variant={'title'} className="">A web starter template for starting apps easily</Typography>
+      <Typography variant={'paragraph'} className="w-1/2">website starter with preconfigured things to bootstrap your site faster</Typography>
+      <div className="flex items-center gap-6">
+        <Button variant={'primary'}>Button</Button>
+        <Button variant={'bordered'}>Button</Button>
+        <Button variant={'secondary'}>Button</Button>
+        <Button variant={'link'}>Button</Button>
+      </div>
     </div>
   );
 }
